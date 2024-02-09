e7e91edb9535e5042f13ba91ebe8c6ee9dddd93b
Brent Lang <bassoon03@gmail.com>
2024-02-09 14:42:39 -0800

    Added API keys

--- WeatherVacationPy/api_keys.py
+++ WeatherVacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "773809a345ab615b99da3d048983a964"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "62ca803795b541d7b32600c93ec5ef73"
